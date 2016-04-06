e317b022bf7829eb048e9cf761b68dcc1bec78b5
kizzlebot <jchoi2012@knights.ucf.edu>
2016-04-06 05:15:19 +0000

    use environment variables when possible, explicitly give config file argument to webpack

--- server.js
+++ server.js
@@ -3,20 +3,26 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
-config.entry.splice(0, 0, 'webpack-dev-server/client?http://localhost:3000', 'webpack/hot/only-dev-server');
+config.entry.splice(0, 0, `webpack-dev-server/client?http://localhost:${process.env.PORT || 3000}`, 'webpack/hot/only-dev-server');
 config.plugins.push(new webpack.HotModuleReplacementPlugin());
 
-var webpackDevServer = new WebpackDevServer(webpack(config), {
+var wp = webpack(config, function(err, stats){
+  if (err) console.log(err);
+  stats.toString({chunks:false});
+});
+
+var webpackDevServer = new WebpackDevServer(wp, {
   publicPath: config.output.publicPath,
   hot: true,
+  quiet:true,
   historyApiFallback: true
 });
 
 
+var port = process.env.PORT || 3000;
 
-webpackDevServer.listen(3000, 'localhost', function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log('Listening at http://localhost:3000/');
+webpackDevServer.listen(port, 'localhost', function (err, result) {
+  if (err) return console.log(err);
+  
+  console.log(`Listening at ${process.env.HOST || 'localhost'}:3000`);
 });
